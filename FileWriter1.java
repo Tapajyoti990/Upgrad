f05acaab6c78d2cd543682677f9182264d2ba7c7
Tapajyoti990 <tapa99unix@gmail.com>
2021-11-04 21:13:13 +0530

    doing

---
+++ FileWriter1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter1 {
+    
+    public static void main(String[] args) throws IOException {
+        FileReader fir = new FileReader("C:/Users/91912/Desktop/udemy web dev/FileHandle/file1.txt");
+        BufferedReader br = new BufferedReader(fir);
+        FileWriter fw = new FileWriter("C:/Users/91912/Desktop/udemy web dev/FileHandle/writer.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        String line=br.readLine();
+        while(line!=null ){
+            bw.write(line);
+            line = br.readLine();
+            
+        }
+        bw.close();
+        fw.close();
+        br.close();
+        fir.close();
+        System.out.println("done");
+    }
+}
