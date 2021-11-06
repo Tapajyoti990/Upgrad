24a2bb364d609fea79fd3f8d60226af91af638c3
Tapajyoti990 <tapa99unix@gmail.com>
2021-11-06 14:51:11 +0530

    Committing feature 1 while loop

---
+++ WhileLoop.java
@@ -0,0 +1,16 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        boolean keepDriving = false;
+        int currentFuel = 50;
+
+        while (currentFuel > 0){
+            keepDriving = true;
+            System.out.println("Keep on driving!");
+            currentFuel--;
+        }
+        int n=Math.po
+        keepDriving = false;
+        System.out.println("Needs to refuel1111111!");
+    }
+}
